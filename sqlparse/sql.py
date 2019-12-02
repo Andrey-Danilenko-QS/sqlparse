b575d32f3c6d88ae2d1fa24f94bf6e5d9127701e
Andrey-Danilenko-QS <andrey.danilenko@quantumsoft.ru>
2019-12-02 13:56:26 +0700

    tried to solve WHERE problem

--- sqlparse/sql.py
+++ sqlparse/sql.py
@@ -553,7 +553,7 @@ class Comment(TokenList):
 
 class Where(TokenList):
     """A WHERE clause."""
-    M_OPEN = T.Keyword, '!!!WHERE'
+    M_OPEN = T.Keyword, '!!!WHERE!!!'
     M_CLOSE = T.Keyword, ()
 
 
